cda7d18836bdb780f64b6930ccb5c6411d2d04c8
AndreyBV5 <andrey0130player@gmail.com>
2025-05-03 16:56:57 -0600

    feat(HEAL-3) visual test

--- healthcare/healthcare/custom/user.py
+++ healthcare/healthcare/custom/user.py
@@ -1,6 +1,7 @@
 import frappe
 
 def assign_default_module_profile(doc, method):
+    frappe.msgprint("Hook ejecutado para asignar módulo por defecto")  # prueba visual
     if not doc.module_profile:
         doc.module_profile = "Default Restricted"
         doc.save(ignore_permissions=True)
