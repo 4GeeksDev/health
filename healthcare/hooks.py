497f81c40fe9b6d10cdf3e0bc9ac60564fc316fe
AndreyBV5 <andrey0130player@gmail.com>
2025-05-03 11:38:10 -0600

    feat(HEAL-3) Remove unnecessary workspace filters from default desktop configuration in hooks.py

--- healthcare/hooks.py
+++ healthcare/hooks.py
@@ -310,11 +310,4 @@ company_data_to_be_ignored = [
 	"Healthcare Service Unit",
 ]
 
-# Oculta workspaces innecesarios del escritorio por defecto
-fixtures = [
-    {"doctype": "Workspace", "filters": [["module", "in", [
-        "Manufacturing", "Projects", "CRM", "Support", "Website", "Education", "Non Profit", "Agriculture"
-    ]]]}
-]
-
 after_install = "healthcare.setup.setup_healthcare"
