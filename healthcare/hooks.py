b141dd775314bbaa38fb2e132b665010f7475b34
AndreyBV5 <andrey0130player@gmail.com>
2025-05-03 15:25:11 -0600

    feat(HEAL-3) Add Module Profile to the list of fixtures in hooks for improved module management and delete unnecessary file

--- healthcare/hooks.py
+++ healthcare/hooks.py
@@ -313,5 +313,6 @@ company_data_to_be_ignored = [
 fixtures = [
     {"doctype": "Workspace", "filters": [["module", "in", [
         "Manufacturing", "Projects", "CRM", "Support", "Website", "Education", "Non Profit", "Agriculture"
-    ]]]}
+    ]]]},
+    "Module Profile"
 ]
