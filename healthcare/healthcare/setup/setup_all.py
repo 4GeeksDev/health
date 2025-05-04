aaa254beb5720720518c070c31c649ef4b7cc5e3
AndreyBV5 <andrey0130player@gmail.com>
2025-05-04 00:38:28 -0600

    feat(HEAL-3) update imports

--- healthcare/healthcare/setup/setup_all.py
+++ healthcare/healthcare/setup/setup_all.py
@@ -1,5 +1,6 @@
-from ..setup_healthcare import setup_healthcare
-from ..module_profile_setup import setup_module_profiles
+from healthcare.setup import setup_healthcare
+from healthcare.module_profile_setup import setup_module_profiles
+
 
 def setup_all():
     setup_healthcare()
