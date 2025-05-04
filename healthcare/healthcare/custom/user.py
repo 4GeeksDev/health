c1f3af016582d44798113d3486469b4fcc94b1d4
AndreyBV5 <andrey0130player@gmail.com>
2025-05-04 02:09:03 -0600

    feat(HEAL-3) update module_profile_name

--- healthcare/healthcare/custom/user.py
+++ healthcare/healthcare/custom/user.py
@@ -1,10 +1,10 @@
 import frappe
 
 def assign_default_module_profile(doc, method):
-    # Verifica si el Module Profile ya existe
+    # Crear Module Profile si no existe
     if not frappe.db.exists("Module Profile", "Default Restricted"):
         profile = frappe.new_doc("Module Profile")
-        profile.name = "Default Restricted"
+        profile.module_profile_name = "Default Restricted"  # <- este es el campo correcto
         profile.restrict_to_domain = ""
         profile.blocked_modules = [
             {"module": "Manufacturing"},
@@ -19,7 +19,7 @@ def assign_default_module_profile(doc, method):
         profile.save(ignore_permissions=True)
         frappe.db.commit()
 
-    # Asignar solo si el usuario no tiene uno definido
+    # Asignar el módulo al usuario si no tiene uno
     if not doc.module_profile:
         doc.module_profile = "Default Restricted"
         doc.save(ignore_permissions=True)
