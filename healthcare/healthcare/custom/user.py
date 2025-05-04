aef9ef9544744390ed6747d42b4a92bd242c31f9
AndreyBV5 <andrey0130player@gmail.com>
2025-05-04 02:03:22 -0600

    feat(HEAL-3) implement default module profile assignment and remove obsolete setup scripts

--- healthcare/healthcare/custom/user.py
+++ healthcare/healthcare/custom/user.py
@@ -1,7 +1,25 @@
 import frappe
 
 def assign_default_module_profile(doc, method):
-    frappe.msgprint("Hook ejecutado para asignar módulo por defecto")  # prueba visual
+    # Verifica si el Module Profile ya existe
+    if not frappe.db.exists("Module Profile", "Default Restricted"):
+        profile = frappe.new_doc("Module Profile")
+        profile.name = "Default Restricted"
+        profile.restrict_to_domain = ""
+        profile.blocked_modules = [
+            {"module": "Manufacturing"},
+            {"module": "Agriculture"},
+            {"module": "Education"},
+            {"module": "Non Profit"},
+            {"module": "Projects"},
+            {"module": "CRM"},
+            {"module": "Support"},
+            {"module": "Website"},
+        ]
+        profile.save(ignore_permissions=True)
+        frappe.db.commit()
+
+    # Asignar solo si el usuario no tiene uno definido
     if not doc.module_profile:
         doc.module_profile = "Default Restricted"
         doc.save(ignore_permissions=True)
--- healthcare/healthcare/setup/setup_all.py
+++
@@ -1,7 +0,0 @@
-# from ..module_profile_setup.setup_module_profiles import setup_module_profiles
-# from ..setup import setup_healthcare
-
-
-# def setup_all():
-#     setup_healthcare()
-#     setup_module_profiles()
