72e98c4848cb2eaab262a52a2bc0089fd70c06dd
AndreyBV5 <andrey0130player@gmail.com>
2025-05-20 01:28:44 -0600

    feat(HEAL-3) remove workspace.json and add new workspace configurations for CRM, Manufacturing, and Projects

--- healthcare/hooks.py
+++ healthcare/hooks.py
@@ -316,3 +316,5 @@ company_data_to_be_ignored = [
 override_workspace = "healthcare/config/workspace.json"
 
 after_install = "healthcare.install.setup_module_profile.create_default_module_profile"
+
+fixtures = ["Workspace"]
