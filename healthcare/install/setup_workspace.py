99e1cbe39320499d612e445bdff14a420ab65c14
AndreyBV5 <andrey0130player@gmail.com>
2025-05-20 15:26:04 -0600

    feat(HEAL-3)  add website workspace configuration and update apply_custom_workspaces function

--- healthcare/install/setup_workspace.py
+++ healthcare/install/setup_workspace.py
@@ -4,17 +4,32 @@ import os
 
 def apply_custom_workspaces():
     workspace_path = frappe.get_app_path("healthcare", "config", "workspace")
-    filenames = ["projects.json", "crm.json", "manufacturing.json", "erpnext_integrations.json", "settings.json"]
+    filenames = [
+        "projects.json",
+        "crm.json",
+        "manufacturing.json",
+        "erpnext_integrations.json",
+        "settings.json",
+        "website.json"
+    ]
 
     for filename in filenames:
         full_path = os.path.join(workspace_path, filename)
         with open(full_path) as f:
             data = json.load(f)
 
-        if frappe.db.exists("Workspace", data.get("name")):
-            frappe.delete_doc("Workspace", data.get("name"), force=True)
+        workspace_name = data.get("name")
 
-        doc = frappe.get_doc(data)
-        doc.flags.ignore_permissions = True
-        doc.flags.ignore_links = True
-        doc.insert(ignore_permissions=True)
+        # Si el Workspace ya existe, actualízalo
+        if frappe.db.exists("Workspace", workspace_name):
+            doc = frappe.get_doc("Workspace", workspace_name)
+            doc.update(data)
+            doc.flags.ignore_permissions = True
+            doc.flags.ignore_links = True
+            doc.save(ignore_version=True)
+        else:
+            # Si no existe, insertarlo como nuevo
+            doc = frappe.get_doc(data)
+            doc.flags.ignore_permissions = True
+            doc.flags.ignore_links = True
+            doc.insert(ignore_permissions=True)
