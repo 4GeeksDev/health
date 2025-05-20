50fae9a8c6482732fc8fff8a63fb1badf53811b6
AndreyBV5 <andrey0130player@gmail.com>
2025-05-20 14:54:23 -0600

    feat(HEAL-3) update apply_custom_workspaces

--- healthcare/install/setup_workspace.py
+++ healthcare/install/setup_workspace.py
@@ -2,7 +2,6 @@ import frappe
 import json
 import os
 
-
 def apply_custom_workspaces():
     workspace_path = frappe.get_app_path("healthcare", "config", "workspace")
     filenames = ["projects.json", "crm.json", "manufacturing.json", "erpnext_integrations.json", "settings.json"]
@@ -12,7 +11,10 @@ def apply_custom_workspaces():
         with open(full_path) as f:
             data = json.load(f)
 
+        if frappe.db.exists("Workspace", data.get("name")):
+            frappe.delete_doc("Workspace", data.get("name"), force=True)
+
         doc = frappe.get_doc(data)
         doc.flags.ignore_permissions = True
         doc.flags.ignore_links = True
-        doc.save(ignore_version=True)
+        doc.insert(ignore_permissions=True)
